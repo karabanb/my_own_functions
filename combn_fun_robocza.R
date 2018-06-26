5251c899d0538247d8e93c96fe71b37d1ac1ef9b
karabanb <b.karaban@me.com>
2018-06-26 15:28:50 +0200

    zamiana nazwy jednej zmiennej i dopisanie fragemtnu odpowiedzialnego za liczenie dni od ostatniego i pierwszego zdarzenia

--- combn_fun_robocza.R
+++ combn_fun_robocza.R
@@ -1,20 +1,20 @@
 
-# library(tidyverse)
-# load("./data/raw_data.Rdata")
-# 
-# x <- raw_data[raw_data$RodzajScore=="PierwszaZewn",]
-# 
-# 
-# date0 <- "Data0"
-# event_date <- "evv_EventDate"
-# event_cols <- c("evt_Code", "evs_Code", "evi_Code", "evr_Code")
-# id <- "cli_ID"
-# intervals <- c(1, 7,180, 360)
+library(tidyverse)
+load("./data/raw_data.Rdata")
+
+x <- raw_data[raw_data$RodzajScore=="PierwszaZewn",]
+
+
+date0 <- "Data0"
+event_date <- "evv_EventDate"
+event_cols <- c("evt_Code", "evs_Code", "evi_Code", "evr_Code")
+id <- "cli_ID"
+intervals <- c(1, 7,180, 360)
 
 
 
 
-count_events <- function(x, id, event_cols, date0, event_date, intervals = c(30, 60, 90, 180)){
+#count_events <- function(x, id, event_cols, date0, event_date, intervals = c(30, 60, 90, 180)){
 
 require(tidyverse)
   
@@ -110,4 +110,4 @@ names(df)[names(df)=="id"] <- id
 
 return(df)
 
-}
+#}
